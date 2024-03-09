e1dfafd4f81e9fb2196be85975c91d09d94d45b2
Araiz Haider <83108709+iaraizhaider@users.noreply.github.com>
2024-03-09 16:55:10 -0500

    Printing Logged In User Information...

--- spring-0.0.1-SNAPSHOT/src/main/java/com/practice/spring/controller/ToDosController.java
+++ spring-0.0.1-SNAPSHOT/src/main/java/com/practice/spring/controller/ToDosController.java
@@ -1,16 +1,19 @@
 package com.practice.spring.controller;
 
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class ToDosController {
 	
 	@RequestMapping("list-todos")
-//	@ResponseBody
 	public String listTodos() {
+		System.out.println("Logged in user is: " + getLoggedInUser());
 		return "list-todos";
 	}
 
+	public String getLoggedInUser() {
+		return SecurityContextHolder.getContext().getAuthentication().getName();
+	}
 }
